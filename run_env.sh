a77281623ab3b9efa8b4cbffba8b57d992fb2208
Micael Lima <micael.filippe@gmail.com>
2021-06-17 22:19:45 -0300

    ajuste na montagem do volume

--- run_env.sh
+++ run_env.sh
@@ -1,1 +1,1 @@
-docker run -v ./:/notebooks --gpus all --rm -it -p 8888:8888 -p 8787:8787 -p 8786:8786 data-science-stack:2.8.0 
+docker run -v ~/experiments:/notebooks --gpus all --rm -it -p 8888:8888 -p 8787:8787 -p 8786:8786 data-science-stack:2.8.0 
